aeb7ad818215825c402613d7ac28fafe50f4b715
minhduy <duyle12697@gmail.com>
2018-03-09 17:54:48 +0700

    Nopbai

---
+++ Bai_1/ConsoleApplication2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strHostName = Dns.GetHostName();
+            Console.WriteLine("Host Name" + strHostName);
+            IPHostEntry iphostentry = Dns.GetHostByName(strHostName);
+            int nIP = 0;
+            foreach (IPAddress ipaddress in iphostentry.AddressList)
+            {
+                Console.WriteLine("IP #" + ++nIP + ": " +ipaddress.ToString());
+            
+            Console.ReadLine();
+            }
+        }
+    }
+}
